f2261e4fd1698d489c6ea49b306f23cbac5da639
lilibetqq <lilibetqq@gmail.com>
2022-09-17 17:38:00 +0400

    check video/photo

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-import cv2 as cv
-import numpy as np
-cap = cv.VideoCapture(0)
+import cv2
 
-while(1):
- 
-    _, frame = cap.read()
-   
-    cv.imshow('frame',frame)
-   
-    k = cv.waitKey(5) & 0xFF
-    if k == 27:
-        break
-cv.destroyAllWindows()
+face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+
+def checkVideo():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        _, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4, 0,  [130, 130])
+        print(faces)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+       
+        cv2.imshow('img', img)
+        k = cv2.waitKey(30) & 0xff
+        if k==27:
+            break
+            
+    cap.release()
+
+def checkPhoto():
+    img = cv2.imread('samples/pasha.jpg')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('img', img)
+    cv2.waitKey()
+
+checkVideo()
+#checkVideo()
